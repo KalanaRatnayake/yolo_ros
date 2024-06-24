7875e654aa8aba2043f2e47958e3d19b8126bc42
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-06-24 15:20:53 +1000

    modifications to dockerfile

--- docker/workspace_entrypoint.sh
+++ docker/workspace_entrypoint.sh
@@ -8,12 +8,4 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "$WORKSPACE_ROOT/install/setup.bash"
 
-ros2 run yolo_ros yolo_ros --ros-args -p "yolo_model:=$YOLO_MODEL" \
-                                      -p "input_topic:=$INPUT_TOPIC" \
-                                      -p "publish_annotated_image:=$PUBLISH_ANNOTATED_IMAGE" \
-                                      -p "output_annotated_topic:=$OUTPUT_ANNOTATED_TOPIC" \
-                                      -p "output_detailed_topic:=$OUTPUT_DETAILED_TOPIC" \
-                                      -p "confidence_threshold:=$CONFIDENCE_THRESHOLD" \
-                                      -p "device:=$DEVICE"
-
 exec "$@"
