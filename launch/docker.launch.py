2bacb07cbb16186572367841bf8e2b45c1e33089
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-07-22 12:51:25 +1000

    minor fix in launch file

--- launch/docker.launch.py
+++ launch/docker.launch.py
@@ -11,16 +11,15 @@ import yaml
 
 def generate_launch_description():
 
-    yolo_model_param                = DeclareLaunchArgument('yolo_model',               default_value="yolov9s.pt")
-    input_rgb_topic_param           = DeclareLaunchArgument('input_rgb_topic',          default_value="/camera/color/image_raw")
-    input_depth_topic_param         = DeclareLaunchArgument('input_depth_topic',        default_value="/camera/depth/points")
-    subscribe_depth_param           = DeclareLaunchArgument('subscribe_depth',          default_value=True)
-    publish_annotated_image_param   = DeclareLaunchArgument('publish_annotated_image',  default_value=True)
-    annotated_topic_param           = DeclareLaunchArgument('annotated_topic',          default_value="/yolo_ros/annotated_image")
-    detailed_topic_param            = DeclareLaunchArgument('detailed_topic',           default_value="/yolo_ros/detection_result")
-    threshold_param                 = DeclareLaunchArgument('threshold',                default_value=0.25)
-    device_param                    = DeclareLaunchArgument('device',                   default_value="0")
-
+    yolo_model_param                = DeclareLaunchArgument(name='yolo_model',               default_value="yolov9s.pt")
+    input_rgb_topic_param           = DeclareLaunchArgument(name='input_rgb_topic',          default_value="/camera/color/image_raw")
+    input_depth_topic_param         = DeclareLaunchArgument(name='input_depth_topic',        default_value="/camera/depth/points")
+    subscribe_depth_param           = DeclareLaunchArgument(name='subscribe_depth',          default_value="True")
+    publish_annotated_image_param   = DeclareLaunchArgument(name='publish_annotated_image',  default_value="True")
+    annotated_topic_param           = DeclareLaunchArgument(name='annotated_topic',          default_value="/yolo_ros/annotated_image")
+    detailed_topic_param            = DeclareLaunchArgument(name='detailed_topic',           default_value="/yolo_ros/detection_result")
+    threshold_param                 = DeclareLaunchArgument(name='threshold',                default_value="0.25")
+    device_param                    = DeclareLaunchArgument(name='device',                   default_value="'0'")
 
     yolo_ros_node = launch_ros.actions.Node(package='yolo_ros',
                                               executable='yolo_ros',
